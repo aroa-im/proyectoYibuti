1d6ac5de72fd2b027044a0b42481dc5218bd3246
aroa-im <aroa.i.m@opendeusto.es>
2025-11-08 14:43:08 +0100

    cambios varios en las clases pelicula,review y videojuego

--- src/domain/Pelicula.java
+++ src/domain/Pelicula.java
@@ -1,58 +1,26 @@
 package domain;
 
-public class Pelicula {
-	private String titulo;
-	private String sinopsis;
-	private float precio;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Pelicula extends Producto{
 	private TipoPelicula tipo;
 	private GeneroPelicula genero;
 	private String director;
-	private Double rating;
 	
-	public Pelicula(String titulo, String sinopsis, float precio, TipoPelicula tipo, GeneroPelicula genero, String director, Double rating) {
-		super();
-		this.titulo = titulo;
-		this.sinopsis = sinopsis;
-		this.precio = precio;
+	public Pelicula(String titulo, String sinopsis, float precio, int rating, ArrayList<Review> comentarios,
+			TipoPelicula tipo, GeneroPelicula genero, String director) {
+		super(titulo, sinopsis, precio, rating, comentarios);
 		this.tipo = tipo;
 		this.genero = genero;
-		this.director= director;
-		this.rating=rating;
+		this.director = director;
 	}
 	
 	public Pelicula() {
 		super();
-		this.titulo = "";
-		this.sinopsis = "";
-		this.precio = 0;
-		this.tipo = TipoPelicula.DVD;
-		this.genero = GeneroPelicula.TERROR;
+		this.tipo = null;
+		this.genero = null;
 		this.director = "";
-		this.rating= 0.0;
-	}
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getSinopsis() {
-		return sinopsis;
-	}
-
-	public void setSinopsis(String sinopsis) {
-		this.sinopsis = sinopsis;
-	}
-
-	public float getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(float precio) {
-		this.precio = precio;
 	}
 
 	public TipoPelicula getTipo() {
@@ -62,7 +30,7 @@ public class Pelicula {
 	public void setTipo(TipoPelicula tipo) {
 		this.tipo = tipo;
 	}
-	
+
 	public GeneroPelicula getGenero() {
 		return genero;
 	}
@@ -70,25 +38,38 @@ public class Pelicula {
 	public void setGenero(GeneroPelicula genero) {
 		this.genero = genero;
 	}
-	
-	public String getAutor() {
-		return autor;
+
+	public String getDirector() {
+		return director;
 	}
 
 	public void setDirector(String director) {
 		this.director = director;
 	}
-	
-	public Double getRating() {
-		return rating;
-	}
-	public void set(Double rating) {
-		this.rating = rating;
-	}
 
 	@Override
 	public String toString() {
-		return "Pelicula [titulo=" + titulo + ", sinopsis=" + sinopsis + ", precio=" + precio + ", tipo=" + tipo + ", genero=" + genero + ", director = " + director + ", rating = " + rating +"]";
+		return "Pelicula [tipo=" + tipo + ", genero=" + genero + ", director=" + director + ", Titulo=" + getTitulo()
+		+ ", Sinopsis=" + getSinopsis() + ", Precio=" + getPrecio() + ", Rating=" + getRating()
+		+ ", Comentarios=" + getComentarios() + "]";
+	}
+
+	@Override
+	public boolean crearAlquiler(Cliente cliente, Producto producto, LocalDate fechaDevolucion) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean avisoAlquilerTarde(Cliente cliente, Producto producto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean terminarAlquiler(Cliente cliente, LocalDate fechaDevolucion) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
