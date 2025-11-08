5db1fe22d8e0543d530acb2515f66c692f890aac
Oier Artabe <oier.artabe@opendeusto.es>
2025-11-08 16:36:04 +0100

    getUsuario y setUsuario

--- src/main/main.java
+++ src/main/main.java
@@ -1,5 +1,19 @@
 package main;
 
-public class main {
+import domain.Usuario;
 
+public class main {
+	
+	private static Usuario usuario;
+	
+	
+	
+	
+	public static Usuario getUsuario() {
+		return usuario;
+	}
+
+	public static void setUsuario(Usuario usuario) {
+		main.usuario = usuario;
+	}
 }
