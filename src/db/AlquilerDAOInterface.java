ddc87cf265b9c6c0bbbc6e0d5b9c78ecef4dc940
aroa-im <aroa.i.m@opendeusto.es>
2025-12-10 15:29:11 +0100

    crear alquiler

---
+++ src/db/AlquilerDAOInterface.java
@@ -0,0 +1,22 @@
+package db;
+
+import java.util.List;
+
+import domain.Alquiler;
+import domain.Cliente;
+
+public interface AlquilerDAOInterface {
+	boolean insertar(Alquiler alquiler);
+	Alquiler obtenerPorId(int id);
+	List<Alquiler> obtenerTodos();
+	List<Alquiler> obtenerPorCliente(String dniCliente);
+	List<Alquiler> obtenerActivosPorCliente(String dniCliente);
+	List<Alquiler> obtenerPorProducto(int idProducto);
+	boolean actualizar(Alquiler alquiler);
+	boolean marcarComoDevuelto(int id);
+	boolean productoEstaAlquilado(int idProducto);
+	List<Alquiler> obtenerAlquileresVencidos();
+	boolean eliminar(int id);
+	Cliente obtenerClienteBasico(String dni);
+	void borrarRegistros();
+}
