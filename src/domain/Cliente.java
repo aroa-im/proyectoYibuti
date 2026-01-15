1ddcfb5755cba325a79116f349426a2613f67635
Oier Artabe <oier.artabe@opendeusto.es>
2026-01-15 19:48:21 +0100

    Cambio en la estructura de cliente

--- src/domain/Cliente.java
+++ src/domain/Cliente.java
@@ -1,65 +1,69 @@
 package domain;
 
 import java.util.ArrayList;
-
 import db.UsuarioDTO;
 import main.main;
 
-public class Cliente extends Usuario{
-	private ArrayList<Producto> historial;
-	private ArrayList<Review> listaReviews;
-	private int amonestaciones;
-	
-	public Cliente(String dni, String nombre, String email, String contrasena, ArrayList<Producto> historial,ArrayList<Review> listaReviews,
-			int amonestaciones) {
-		super(dni, nombre, email, contrasena);
-		this.historial = historial;
-		this.amonestaciones = amonestaciones;
-	}
-	
-	public Cliente() {
-		super();
-		this.historial = new ArrayList<Producto>();
-		this.listaReviews = new ArrayList<Review>();
-		this.amonestaciones = 0;
-	}
-	public Cliente(UsuarioDTO usuarioDTO) {
-		super(usuarioDTO);
-		this.historial = new ArrayList<>();
-		this.listaReviews = main.getReviewDAO().getReviewsByUsuarioDni(usuarioDTO.getDni());
-		this.amonestaciones = usuarioDTO.getAmonestaciones();
-	}
-	public ArrayList<Producto> getHistorial() {
-		return historial;
-	}
-
-	public void setHistorial(ArrayList<Producto> historial) {
-		this.historial = historial;
-	}
-	public ArrayList<Review> getListaReviews() {
-		return listaReviews;
-	}
-
-	public void setListaReviews(ArrayList<Review> listaReviews) {
-		this.listaReviews = listaReviews;
-	}
-
-
-	public int getAmonestaciones() {
-		return amonestaciones;
-	}
-
-	public void setAmonestaciones(int amonestaciones) {
-		this.amonestaciones = amonestaciones;
-	}
-
-	@Override
-	public String toString() {
-		return "Cliente [historial=" + historial + ", listaReviews=" + listaReviews + ", amonestaciones="
-				+ amonestaciones + "]";
-	}
-
-	
-	
+public class Cliente extends Usuario {
+    private ArrayList<Producto> historial;
+    private ArrayList<Review> listaReviews;
+    private int amonestaciones;
+    
+    public Cliente(String dni, String nombre, String email, String contrasena, 
+                   ArrayList<Producto> historial, ArrayList<Review> listaReviews,
+                   int amonestaciones) {
+        super(dni, nombre, email, contrasena);
+        this.historial = historial;
+        this.listaReviews = listaReviews;
+        this.amonestaciones = amonestaciones;
+    }
+    
+    public Cliente() {
+        super();
+        this.historial = new ArrayList<Producto>();
+        this.listaReviews = new ArrayList<Review>();
+        this.amonestaciones = 0;
+    }
+    
+    public Cliente(UsuarioDTO usuarioDTO) {
+        super(usuarioDTO);
+        
+        // Cargar historial de productos alquilados desde la BD
+        this.historial = main.getAlquilerDAO().getProductosAlquiladosByUsuario(usuarioDTO.getDni());
+        
+        // Cargar reviews del usuario
+        this.listaReviews = main.getReviewDAO().getReviewsByUsuarioDni(usuarioDTO.getDni());
+        
+        this.amonestaciones = usuarioDTO.getAmonestaciones();
+    }
+    
+    public ArrayList<Producto> getHistorial() {
+        return historial;
+    }
+    
+    public void setHistorial(ArrayList<Producto> historial) {
+        this.historial = historial;
+    }
+    
+    public ArrayList<Review> getListaReviews() {
+        return listaReviews;
+    }
+    
+    public void setListaReviews(ArrayList<Review> listaReviews) {
+        this.listaReviews = listaReviews;
+    }
+    
+    public int getAmonestaciones() {
+        return amonestaciones;
+    }
+    
+    public void setAmonestaciones(int amonestaciones) {
+        this.amonestaciones = amonestaciones;
+    }
+    
+    @Override
+    public String toString() {
+        return "Cliente [historial=" + historial + ", listaReviews=" + listaReviews + 
+               ", amonestaciones=" + amonestaciones + "]";
+    }
 }
-
